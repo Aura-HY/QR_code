a6bbc64e81f5c9752fe8d39771e2c38161ef2d72
Aura-HY <144097076+Aura-HY@users.noreply.github.com>
2024-07-05 00:11:05 +0800

    页面创建

---
+++ src/pages/detailShow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >sdhfks</div>
+</template>
+
+<script>
+import user from '@/api/user.js'
+export default {
+    data() {
+        return {
+            userInfo: {}
+        }
+    },
+    mounted() {
+        this.getUserInfo()
+    },
+    methods: {
+        getUserInfo() {
+            user.getUserInfo().then(res => {
+                this.userInfo = res.data
+            })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
